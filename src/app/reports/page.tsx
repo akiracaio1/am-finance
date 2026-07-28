4498a8cad7b98cd3f623207e08c3fe0b0c5e43f9
Caio Akira <akiracaio1@gmail.com>
2026-07-28 20:18:41 +0000

    Try fixing this error: `Runtime ReferenceError: Badge is not defined. Er

--- src/app/reports/page.tsx
+++ src/app/reports/page.tsx
@@ -29,6 +29,7 @@ import {
 } from "@/components/ui/select";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { Badge } from "@/components/ui/badge";
 import { 
   useUser, 
   useFirestore, 
